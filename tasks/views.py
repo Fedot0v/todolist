5d1c124fce27d4f1d8daf4539d38fa9053faeb39
mykyta <nikita.fedotov222@gmail.com>
2024-08-21 11:56:51 +0300

    create view lists

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from tasks.models import Tag, Task
+
+
+def index(request):
+    return render(request, "tasks/index.html")
+
+
+class TagViewList(ListView):
+    model = Tag
+    template_name = "tasks/tags_list.html"
+    context_object_name = "tags"
+
+
+class TaskViewList(ListView):
+    model = Task
+    template_name = "tasks/tasks_list.html"
+    context_object_name = "tasks"
+    ordering = ["is_complete", "created_at"]
