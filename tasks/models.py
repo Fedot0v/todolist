8487cc00ec4a6a2d9ecfbaa8000ee1bce64e2a01
mykyta <nikita.fedotov222@gmail.com>
2024-08-21 11:52:19 +0300

    add models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_complete = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return f"Tag: {self.tags.name} content: {self.content}"
+
+
