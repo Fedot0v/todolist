3d2a071757f1f0f24bc835f64dc90622e1b13e75
mykyta <nikita.fedotov222@gmail.com>
2024-08-21 15:56:04 +0300

    add tasks create and tasks delete paths

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,14 +4,17 @@ from tasks import views
 from tasks.views import (
     TaskViewList,
     TagViewList,
-    TagCreateView, TagUpdateView, TagDeleteView
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TaskCreateView,
+    TaskUpdateView, TaskDeleteView
 )
 
 app_name = "tasks"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("tasks", TaskViewList.as_view(), name="tasks-list"),
+    path("", TaskViewList.as_view(), name="index"),
     path("tags", TagViewList.as_view(), name="tags-list"),
     path("tags/create/", TagCreateView.as_view(), name="tags-create"),
     path(
@@ -24,4 +27,16 @@ urlpatterns = [
         TagDeleteView.as_view(),
         name="tags-delete"
     ),
+    path("task/create", TaskCreateView.as_view(), name="tasks-create"),
+    path(
+        "task/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="tasks-update"
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="tasks-delete"
+    ),
+
 ]
