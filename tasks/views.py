11d9cd1b3bf4bb73b1155e91c93896c3fe389e18
mykyta <nikita.fedotov222@gmail.com>
2024-08-21 13:16:55 +0300

    create tag create view

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from tasks.models import Tag, Task
 
@@ -14,8 +14,16 @@ class TagViewList(ListView):
     context_object_name = "tags"
 
 
+class TagCreateView(CreateView):
+    model = Tag
+    template_name = "tasks/tag_form.html"
+    context_object_name = "tag"
+    success_url = "tasks:tags-list"
+
+
 class TaskViewList(ListView):
     model = Task
-    template_name = "tasks/tasks_list.html"
+    template_name = "tasks/index.html"
     context_object_name = "tasks"
     ordering = ["is_complete", "created_at"]
+    paginate_by = 5
