0eb1c7b17edf784bb7dd7baa95df7260a73f6fe2
mykyta <nikita.fedotov222@gmail.com>
2024-08-21 13:28:46 +0300

    add tag update path

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from tasks import views
-from tasks.views import TaskViewList, TagViewList
+from tasks.views import (
+    TaskViewList,
+    TagViewList,
+    TagCreateView, TagUpdateView
+)
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("tasks", TaskViewList.as_view(), name="tasks-list"),
-    path("tags", TagViewList.as_view(), name="tags=list"),
+    path("tags", TagViewList.as_view(), name="tags-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tags-form"),
+    path("tags/<int:pk>/create/", TagUpdateView.as_view(), name="tags-form"),
 ]
